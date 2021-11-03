3ecf8292b10ad391bb11a994df94c1d7baecff01
Nicolas Castello Luro <nicolas.castelloluro@mercadolibre.com>
2021-11-03 15:56:52 -0300

    commit

---
+++ NotBored1/NotBored/InitialViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InitialViewController.swift
+//  NotBored
+//
+//  Created by Joaquin Diego Borsari Marchese on 3/11/21.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
